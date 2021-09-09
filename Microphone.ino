cead36c7f7b2a3d17288b97ebd94bea37f6ebae6
cryonayes <cryonayes@gmail.com>
2021-09-09 13:30:57 +0300

    Update

--- Microphone.ino
+++ Microphone.ino
@@ -1,15 +1,26 @@
 #include <FastLED.h>
+#include <OneButton.h>
 
 #define NUM_LEDS 28
 #define DATA_PIN 2
+#define COLOR_PIN A2
 #define MIC_PIN A0
 
+#define BUTTON_PIN 5
+#define SHORT_PRESS 500
+#define LONG_PRESS 1000
+
 CRGBArray<NUM_LEDS> leds;
+OneButton animationButton(BUTTON_PIN, true, true);
+
+volatile uint32_t color = 0;
+volatile uint8_t selectedAnimation = 0;
 
 enum animationTypes {
-  ANIMATION_NONE = 0,
-  ANIMATION_RAINBOW,
-  ANIMATION_SOLID
+  ANIMATION_RAINBOW = 0,
+  ANIMATION_RAINBOW_MIDDLE = 1,
+  ANIMATION_SOLID_TRAIL = 2,
+  ANIMATION_SOLID_TRAIL_DOUBLE = 3
 };
 
 int read_mic() {
@@ -28,7 +39,7 @@ void rainbow() {
         return;
     }
 
-    fill_rainbow(leds, mappedValue, 100, 20);
+    fill_rainbow(leds, mappedValue, color, 20);
 }
 
 void rainbow_middle() {
@@ -40,7 +51,7 @@ void rainbow_middle() {
         return;
     }
 
-    fill_rainbow(&leds[NUM_LEDS / 2 - 1], mappedValue / 2, 120, 5);
+    fill_rainbow(&leds[NUM_LEDS / 2 - 1], mappedValue / 2, color, 2);
 
     for(int i = (NUM_LEDS / 2 - 1); i >= 0; i--) { 
         if (i == 0) {
@@ -72,7 +83,7 @@ void solid_trail() {
         }
     }
 
-    fill_solid(leds, mappedValue, CRGB::Red);
+    fill_solid(leds, mappedValue, CRGB(CHSV{color, color / 2, color / 2}));
     calledTime++;
 }
 
@@ -88,7 +99,7 @@ void solid_trail2() {
         leds[lastHead] = CRGB::White;
         leds[lastHead - 1] = CRGB::White;
     }else {
-        if (calledTime % 10 == 0) {
+        if (calledTime % 50 == 0) {
             calledTime = 0;
             leds[lastHead] = CRGB::Black;
             leds[lastHead - 1] = CRGB::Black;
@@ -102,9 +113,28 @@ void solid_trail2() {
     calledTime++;
 }
 
+void handleButton(const byte newState, const unsigned long interval, const byte whichPin) {
+    switch (whichPin) {
+        case BUTTON_PIN:
+            if (newState == LOW) {
+                if (interval <= SHORT_PRESS) {
+                    selectedAnimation++;
+                    if(selectedAnimation % 4 == 0) { selectedAnimation = 0; }
+                }else if(interval >= LONG_PRESS) {
+                    selectedAnimation++;
+                    if(selectedAnimation % 4 == 0) { selectedAnimation = 0; }
+                }
+            }
+    }
+}
+
 void setup() {
     pinMode(MIC_PIN, INPUT);
+    pinMode(COLOR_PIN, INPUT);
     Serial.begin(9600);
+
+    animationButton.attachLongPressStop(onButtonLongPress);
+    animationButton.attachClick(onButtonShortPress);
 
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
     FastLED.setBrightness(10);
@@ -112,8 +142,32 @@ void setup() {
     FastLED.show();
 }
 
+void onButtonShortPress() {
+    selectedAnimation++;
+    if (selectedAnimation % 4 == 0) { selectedAnimation = 0; }
+}
+
+void onButtonLongPress() {}
+
 void loop() {
-    solid_trail2();
+    animationButton.tick();
+    color = analogRead(COLOR_PIN);
+    color = map(color, 0, 1023, 0, 255);
+
+    switch(selectedAnimation) {
+        case animationTypes::ANIMATION_RAINBOW:
+            rainbow();
+            break;
+        case animationTypes::ANIMATION_RAINBOW_MIDDLE:
+            rainbow_middle();
+            break;
+        case animationTypes::ANIMATION_SOLID_TRAIL:
+            solid_trail();
+            break;
+        case animationTypes::ANIMATION_SOLID_TRAIL_DOUBLE:
+            solid_trail2();
+            break;
+    }
+    
     FastLED.show();
-    delay(5);
 }
