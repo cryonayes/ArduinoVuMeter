8eff67a5ec2ceefe1daebe1b61f290b022b1ad28
cryonayes <cryonayes@gmail.com>
2021-09-10 23:29:57 +0300

    New

--- Microphone.ino
+++ Microphone.ino
@@ -1,165 +1,99 @@
-#include <FastLED.h>
+#include <LEDController.h>
 #include <OneButton.h>
 
-#define NUM_LEDS 28
-#define DATA_PIN 2
-#define SENSITIVITY A4
-#define MIC_PIN A0
-
+#define MIC_PIN A4
+#define SENSITIVITY A0
+#define LED_COUNT 28
+#define LED_PIN 2
 #define BUTTON_PIN 5
-#define SHORT_PRESS 500
-#define LONG_PRESS 1000
 
-CRGBArray<NUM_LEDS> leds;
+typedef LEDController<LED_COUNT, LED_PIN> MyController;
+MyController* myController;
+
 OneButton animationButton(BUTTON_PIN, true, true);
 
-CRGB color(100, 100, 100);
-uint8_t brightness = 10;
-uint8_t selectedAnimation = 0;
+void onButtonShortPress() {
+    myController->setColor(random8(255), random8(255), random8(255));
+}
 
-enum AnimationTypes {
-  ANIMATION_RAINBOW = 0,
-  ANIMATION_RAINBOW_MIDDLE = 1,
-  ANIMATION_SOLID_TRAIL = 2,
-  ANIMATION_SOLID_TRAIL_DOUBLE = 3
-};
+void onButtonLongPress() {
+    static int animationIndex = 0;
+    animationIndex++;
+    if (animationIndex % 6 == 0) { animationIndex = 0; }
+
+    if(animationIndex == 1) {
+        myController->changeAnimation(AnimationTypes::ANIMATION_RAINBOW);
+    }else if(animationIndex == 2) {
+        myController->changeAnimation(AnimationTypes::ANIMATION_RAINBOW_CYCLE);
+    }else if(animationIndex == 3) {
+        myController->changeAnimation(AnimationTypes::ANIMATION_RAINBOW_MIDDLE);
+    }else if(animationIndex == 4) {
+        myController->changeAnimation(AnimationTypes::ANIMATION_RAINBOW_MIDDLE_CYCLE);
+    }else if(animationIndex == 5) {
+        myController->changeAnimation(AnimationTypes::ANIMATION_SOLID_TRAIL);
+    }
+}
+
+void onButtonDoubleClick() {
+    static int doubleClickIndex = 0;
+    if (doubleClickIndex == 0) {
+        myController->setColor(0, 255, 0);
+    }else if (doubleClickIndex == 1) {
+        myController->setColor(0, 0, 255);
+    }else if (doubleClickIndex == 2) {
+        myController->setColor(255, 0, 0);
+    }
+    doubleClickIndex++;
+
+    if (doubleClickIndex % 3 == 0) {
+        doubleClickIndex = 0;
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    LEDControllerSettings settings = {
+        .ledPIN = 2,
+        .brightness = 1,
+        .delay = 0,
+        .animationSettings = {
+            .rainbowStep = 12,
+            .peakTrailSize = 2,
+            .countTrailStep = 20,
+            .delayTrail = 0
+        },
+        .powerSettings = {
+            .volt = 5,
+            .miliamps = 50
+        }
+    };
+    myController = &MyController::getInstance(settings);
+    myController->changeAnimation(AnimationTypes::ANIMATION_RAINBOW_MIDDLE);
+
+    animationButton.attachLongPressStop(onButtonLongPress);
+    animationButton.attachClick(onButtonShortPress);
+    animationButton.attachDoubleClick(onButtonDoubleClick);
+}
 
 int read_mic() {
     int value = analogRead(MIC_PIN);
-    int sensitivity = analogRead(SENSITIVITY);
-    sensitivity = map(sensitivity, 0, 1024, 0, 512);
+    int sensitivity = 0; //analogRead(SENSITIVITY);
+    sensitivity = map(sensitivity, 0, 1025, 0, 512);
 
-    value = abs(value - 512); // max 450
     value = (value <= sensitivity) ? 0 : (value - sensitivity);
     return value;
 }
 
-void rainbow() {
-    int value = read_mic();
-    int mappedValue = map(value, 0, 450, 0, NUM_LEDS);
-
-    if(mappedValue <= 0) {
-        fill_solid(leds, NUM_LEDS, CRGB::Black);
-        return;
-    }
-
-    fill_rainbow(leds, mappedValue, color, 12);
-}
-
-void rainbow_middle() {
-    int value = read_mic();
-    int mappedValue = map(value, 0, 450, 0, NUM_LEDS);
-
-    if(mappedValue <= 0) {
-        fill_solid(leds, NUM_LEDS, CRGB::Black);
-        return;
-    }
-
-    fill_rainbow(&leds[NUM_LEDS / 2 - 1], mappedValue / 2 + 1, rgb2hsv_approximate(color).hue, 12);
-
-    for(int i = (NUM_LEDS / 2 - 1); i >= 0; i--) { 
-        if (i == 0) {
-            leds[i] = leds[NUM_LEDS - 1];
-            break;
-        }
-
-        leds[i] = leds[NUM_LEDS - i];
-    }
-
-}
-
-void solid_trail() {
-    static int calledTime = 0;
-    static int lastHead = 0;
-
-    int value = read_mic();
-    int mappedValue = map(value, 0, 512, 0, NUM_LEDS);
-
-    if (mappedValue > lastHead) {
-        lastHead = mappedValue;
-        leds[lastHead].setRGB(255,255,255);
-    }else {
-        if (calledTime % 20 == 0) {
-            calledTime = 0;
-            leds[lastHead].setRGB(0,0,0);
-            lastHead = lastHead - 1;
-            leds[lastHead].setRGB(255,255,255);
-        }
-    }
-
-    fill_solid(leds, mappedValue, color);
-    calledTime++;
-}
-
-void solid_trail2() {
-    static int calledTime = 0;
-    static int lastHead = 0;
-
-    int value = read_mic();
-    int mappedValue = map(value, 0, 512, 0, NUM_LEDS);
-
-    if (mappedValue > lastHead) {
-        lastHead = mappedValue;
-        leds[lastHead].setRGB(255,255,255);
-        leds[lastHead - 1].setRGB(255,255,255);
-    }else {
-        if (calledTime % 20 == 0) {
-            calledTime = 0;
-            leds[lastHead].setRGB(0,0,0);
-            leds[lastHead - 1].setRGB(0,0,0);
-            lastHead = lastHead - 1;
-            leds[lastHead].setRGB(255,255,255);
-            leds[lastHead - 1].setRGB(255,255,255);
-        }
-    }
-
-    fill_solid(leds, mappedValue, color);
-    calledTime++;
-}
-
-void setup() {
-    pinMode(MIC_PIN, INPUT);
-    pinMode(SENSITIVITY, INPUT);
-    // Serial.begin(9600);
-
-    animationButton.attachLongPressStop(onButtonLongPress);
-    animationButton.attachClick(onButtonShortPress);
-
-    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-    FastLED.setBrightness(10);
-    FastLED.clear();
-    FastLED.show();
-}
-
-void onButtonShortPress() {
-    selectedAnimation++;
-    if (selectedAnimation % 4 == 0) { selectedAnimation = 0; }
-    FastLED.clear();
-}
-
-void onButtonLongPress() {
-    color = CRGB(random(255), random(255), random(255));
-}
-
 void loop() {
     animationButton.tick();
+    int micVal = read_mic();
+    int ledVal = map(micVal, 0, 1025, 0, 29);
 
-    switch(selectedAnimation) {
-        case AnimationTypes::ANIMATION_RAINBOW:
-            rainbow();
-            break;
-        case AnimationTypes::ANIMATION_RAINBOW_MIDDLE:
-            rainbow_middle();
-            break;
-        case AnimationTypes::ANIMATION_SOLID_TRAIL:
-            solid_trail();
-            break;
-        case AnimationTypes::ANIMATION_SOLID_TRAIL_DOUBLE:
-            solid_trail2();
-            break;
-        default:
-            break;
+    myController->update(ledVal);
+
+    if(myController->getAnimation() == AnimationTypes::ANIMATION_RAINBOW_CYCLE || myController->getAnimation() == AnimationTypes::ANIMATION_RAINBOW_MIDDLE_CYCLE) {
+        EVERY_N_MILLISECONDS(20) {
+            myController->setRainbowColor(myController->getRainbowColor().hue + 5 % 255);
+        }
     }
-    FastLED.delay(5);
-    FastLED.show();
 }
